3290aa8ad5a3d14d39b55327a79d534f35ad5e30
vchang2146 <vchang2146@student.pps.net>
2024-05-09 10:00:21 -0700

    First Commit

---
+++ whatfruitami.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    "which one do you relate to more?",
+    "answer1": "i'm more shy and it takes me awhile to warm up to people.",
+    "answer1Total": "1",
+    "answer2": "i'm really outgoing and I like to talk to people even if it's complete strangers",
+    "answer2Total": "2",
+    "answer3": "it depends on my mood sometimes i like to go out and sometimes i like to stay inside",
+    "answer3Total": "3"
+    "answer4": "i enjoy spending time with my closest friends and my inner circle the most",
+    "answer4Total": "4"
+  },
